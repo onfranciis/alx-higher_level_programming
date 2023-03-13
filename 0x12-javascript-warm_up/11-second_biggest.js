76a778b6199e89157b9793633ef69e950eea7789
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-03-14 00:07:21 +0100

    feat: create 11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const array = process.argv.slice(2);
+if (array.length < 2) {
+  console.log(0);
+} else {
+  const sortedArray = array.sort((a, b) => b - a);
+  console.log(sortedArray[0]);
+}
