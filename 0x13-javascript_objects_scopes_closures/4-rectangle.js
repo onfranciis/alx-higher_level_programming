0d61e2ef09901175b014706afef690dea570ee33
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-03-14 20:47:57 +0100

    fix: update code

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,40 +1,40 @@
 #!/usr/bin/node
-module.exports =
-  class Rectangle {
-    constructor (w, h) {
-      if ((w > 0) && (h > 0)) {
-        this.width = w;
-        this.height = h;
-      }
+export
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
+  }
 
-    print () {
-      for (let i = 0; i < this.height; i++) {
-        let value = '';
-        for (let j = 0; j < this.width; j++) {
-          value += 'X';
-        }
-        console.log(value);
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let value = '';
+      for (let j = 0; j < this.width; j++) {
+        value += 'X';
       }
+      console.log(value);
     }
+  }
 
-    rotate () {
-      for (let i = 0; i < (this.width * 2); i++) {
-        let value = '';
-        for (let j = 0; j < (this.height * 2); j++) {
-          value += 'X';
-        }
-        console.log(value);
+  rotate () {
+    for (let i = 0; i < (this.width * 2); i++) {
+      let value = '';
+      for (let j = 0; j < (this.height * 2); j++) {
+        value += 'X';
       }
+      console.log(value);
     }
+  }
 
-    double () {
-      for (let i = 0; i < (this.height * 2); i++) {
-        let value = '';
-        for (let j = 0; j < (this.width * 2); j++) {
-          value += 'X';
-        }
-        console.log(value);
+  double () {
+    for (let i = 0; i < (this.height * 2); i++) {
+      let value = '';
+      for (let j = 0; j < (this.width * 2); j++) {
+        value += 'X';
       }
+      console.log(value);
     }
-  };
+  }
+}
