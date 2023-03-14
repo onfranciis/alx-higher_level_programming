b913f7dfd1e1811aae0f48dd8cbebee993f59304
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-03-14 02:29:22 +0100

    fix: update code

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,9 +3,9 @@ const number = parseInt(process.argv[2]);
 if (Number.isNaN(number)) {
   console.log(1);
 } else {
-  let value = 0;
+  let value = 1;
   for (let i = 1; i <= number; i++) {
-    value += i;
+    value *= i;
   }
   console.log(value);
 }
