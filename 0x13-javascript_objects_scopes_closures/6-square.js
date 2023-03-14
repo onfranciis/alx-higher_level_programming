11df55ff31db4e96a155cc83d0f75b6aa9769dba
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-03-14 23:53:58 +0100

    fix: update code

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,13 +5,12 @@ const SquareFirst = require('./5-square');
 module.exports = class Square extends SquareFirst {
   constructor (c) {
     super(c);
-    this.c = c;
   }
 
   charPrint (character) {
     if (character) {
-      for (let i = 0; i < this.c; i++) {
-        console.log(character.repeat(this.c));
+      for (let i = 0; i < this.height; i++) {
+        console.log(character.repeat(this.height));
       }
     } else {
       this.print();
