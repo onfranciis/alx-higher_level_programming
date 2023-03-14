805406df4bc88aa5d2355e38f78836ac2a1b1dea
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-03-14 03:08:01 +0100

    fix: update code

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,13 @@
 #!/usr/bin/node
-const number = parseInt(process.argv[2]);
-let value = 1;
-for (let i = 1; i <= number; i++) {
-  value *= i;
+// const number = parseInt(process.argv[2])
+// let value = 1
+// for (let i = 1; i <= number; i++) {
+//   value *= i
+// }
+// console.log(value)
+
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
-console.log(value);
+
+console.log(factorial(Number(process.argv[2])));
